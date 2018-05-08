421562db4169dbbb554e8c7652ae38ad7e57f2db
Ben Biggs <bebiggs@cisco.com>
2018-05-08 12:06:57 -0500

    feat(ListItemHeader): created and added styles

--- src/lib/index.js
+++ src/lib/index.js
@@ -36,6 +36,7 @@ export { default as InputHelper } from './InputHelper';
 export { default as Label } from './Label';
 export { default as List } from './List';
 export { default as ListItem } from './ListItem';
+export { default as ListItemHeader } from './ListItemHeader';
 export { default as ListItemSection } from './ListItemSection';
 export { default as ListSeparator } from './ListSeparator';
 export { default as Loading } from './Loading';
